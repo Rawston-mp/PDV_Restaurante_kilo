4109392014e245d74258a47c69a5098f7f879415
Rawston-mp <rawstonmpinto@gmail.com>
2026-06-04 17:14:46 -0300

    feat(produtos): persistir custo/margem e melhorar espacos do cadastro

--- src/modules/products/application/dto/CreateProductInput.ts
+++ src/modules/products/application/dto/CreateProductInput.ts
@@ -1,7 +1,10 @@
 export type CreateProductInput = {
   id: string;
+  productCode: string;
   name: string;
   category: string;
+  costValue: number;
+  marginProfit: number;
   price: number;
   byWeight: boolean;
   stock: number;
